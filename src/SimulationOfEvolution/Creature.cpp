c51921cfb590b596d34b2d0f76551c0496907d32
Jochman Radoslav <jochmanr@natur.cuni.cz>
2024-04-13 16:29:06 +0200

    Start of creature implementation. Implemented random genome generation and started working on function that will build the brain according to genome.

--- src/SimulationOfEvolution/Creature.cpp
+++ src/SimulationOfEvolution/Creature.cpp
@@ -1,5 +1,9 @@
 #pragma once
 #include "Creature.h"
+#include <bitset>
+
+//enum class ActionNeuronsType { MFR, Mrn, MRL, Mx, My, Kill };
+//enum class SensorNeuronsType { Age, Rnd, BDy, BD, Lx, Ly, Osc };
 
 void Creature::createGenome(std::size_t numOfGenes)
 {
@@ -18,4 +22,39 @@ void Creature::createGenome(std::size_t numOfGenes)
 		genome.push_back(gene.str());
 	}
 
+}
+
+void Creature::buildBrain()
+{
+	
+	for (auto&& gene : genome)
+	{
+		std::stringstream ss;
+		unsigned tmp;
+		ss << gene;
+		ss >> tmp;
+		std::string binGenome = std::bitset<32>(tmp).to_string();
+		switch (binGenome[0])
+		{
+		case 0:
+		{
+			SensorNeuronsType neuronType = static_cast<SensorNeuronsType>(std::stoi(binGenome.substr(1, 8),nullptr,2)%((int)SensorNeuronsType::size)+1);
+			
+		}
+		default:
+			break;
+		}
+	}
+}
+
+void Creature::addSensorNeuron(SensorNeuronsType type)
+{
+	switch (type)
+	{
+	case SensorNeuronsType::Age:
+	{
+		sensorBrain.emplace(std::make_pair(type, AgeNeuron()));
+	}
+
+	}
 }
--- src/SimulationOfEvolution/Creature.h
+++ src/SimulationOfEvolution/Creature.h
@@ -4,11 +4,20 @@
 #include <ranges>
 #include <sstream>
 #include <vector>
+#include <map>
+#include "Neurons.h"
+
+enum class ActionNeuronsType { MFR, Mrn, MRL, Mx, My, Kill, size};
+enum class SensorNeuronsType { Age, Rnd, BDy, BD, Lx, Ly, Osc, size};
+
 class Creature
 {
 	std::vector<std::string> genome;
-
+	std::map<SensorNeuronsType, SensorNeuron> sensorBrain;
+	std::map<int, InternalNeuron> internalBrain;
+	std::map<ActionNeuronsType, std::unique_ptr<ActionNeuron>> actionBrain;
 private:
 	void createGenome(std::size_t numOfGenes);
 	void buildBrain();
+	void addSensorNeuron(SensorNeuronsType type);
 };
