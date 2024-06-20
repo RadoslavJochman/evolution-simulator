bc2c5ac1db94b703492b26663ff708b6c95d4d1e
Jochman Radoslav <jochmanr@natur.cuni.cz>
2024-06-20 12:18:36 +0200

    Fixed hexToBin function

--- src/SimulationOfEvolution/utilites.h
+++ src/SimulationOfEvolution/utilites.h
@@ -7,14 +7,28 @@
 #include <unordered_map>
 #include "Neurons.h"
 
-template<std::size_t hexSize>
-std::string hexToBin(const std::string& hexNumber)
-{
-	std::stringstream ss;
-	unsigned tmp;
-	ss << hexNumber;
-	ss >> tmp;
-	return std::bitset<4*hexSize>(tmp).to_string();
+inline std::string hexToBin(const std::string& hex) {
+    static const std::unordered_map<char, std::string> hexToBinMap = {
+        {'0', "0000"}, {'1', "0001"}, {'2', "0010"}, {'3', "0011"},
+        {'4', "0100"}, {'5', "0101"}, {'6', "0110"}, {'7', "0111"},
+        {'8', "1000"}, {'9', "1001"}, {'A', "1010"}, {'B', "1011"},
+        {'C', "1100"}, {'D', "1101"}, {'E', "1110"}, {'F', "1111"},
+        {'a', "1010"}, {'b', "1011"}, {'c', "1100"}, {'d', "1101"},
+        {'e', "1110"}, {'f', "1111"}
+    };
+
+    std::string bin;
+    for (char hexChar : hex) {
+        if (hexToBinMap.find(hexChar) != hexToBinMap.end()) {
+            bin += hexToBinMap.at(hexChar);
+        }
+        else
+        {
+
+        }
+    }
+
+    return bin;
 }
 
 struct Config
